291c1538288252d8d21e81000982e781af0cd59f
Noelia López Galiana <124814658+LadyAnaila@users.noreply.github.com>
2023-11-22 18:18:50 +0100

    carpeta dist

---
+++ PEC3/PEC3_Ej3/src/dist/ejercicio1.js
@@ -0,0 +1,25 @@
+//Sustituye /***/ por las instrucciones adeacuadas que cumplan las operaciones  y salidas indicadas en los comentarios.
+function printArray(array) {
+    console.log(array.join(','));
+}
+var array = [2, 3, 4];
+console.log(array[0]); // 2
+printArray(array.slice(1)); // 3,4
+console.log(array[1], array[2]); // 3 4
+array.push(5);
+printArray(array); // 3,4,5
+console.log(array[array.length - 1]); // 5
+array.pop();
+printArray(array); // 3,4
+array.push(1);
+printArray(array); // 3,4,1
+array.unshift(8);
+printArray(array); // 8,3,4,1
+/** check if every number is greater than 3 */
+var everyisgreater = array.every(function (num) { return num > 3; });
+console.log(everyisgreater); // false
+/** check if every number is less than 10 */
+var everyisless = array.every(function (num) { return num < 10; });
+console.log(everyisless); // true
+console.log(array.slice().sort()); // 1,3,4,8
+console.log(array.slice().sort(function (a, b) { return b - a; })); // 8,4,3,1
