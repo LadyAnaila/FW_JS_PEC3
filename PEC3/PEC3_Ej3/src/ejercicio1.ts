84714ec934ab570de5124b59290da159704e38a1
Noelia López Galiana <124814658+LadyAnaila@users.noreply.github.com>
2023-11-22 17:58:14 +0100

    actualización ejercicio1.ts

--- PEC3/PEC3_Ej3/src/ejercicio1.ts
+++ PEC3/PEC3_Ej3/src/ejercicio1.ts
@@ -1,28 +1,36 @@
-Sustituye /***/ por las instrucciones adeacuadas que cumplan las operaciones 
-y salidas indicadas en los comentarios.
+//Sustituye /***/ por las instrucciones adeacuadas que cumplan las operaciones  y salidas indicadas en los comentarios.
 
 
-function printArray(array:Array<number>):void{
-	//code to print the array on console
-       /***/
-}
-
-let array:number[]=[2,3,4];
-console.log(/***/); //2
-printArray(array); // 3,4
-/***/
-printArray(array); // 3,4,5
-console.log(/***/); //5
-printArray(array); // 3,4
-/***/
-printArray(array); // 3,4,1
-/***/
-printArray(array); // 8,3,4,1
-/** check if every number is greater than 3 */
-let everyisgreater = /***/;
-console.log(everyisgreater);  //false
-/** check if every number is less than 10 */
-let everyisless = /***/;
-console.log(everyisless);  //true
-console.log(array.sort()); //1,3,4,8
-console.log(/***/); //8,4,3,1
+function printArray(array: Array<number>): void {
+       console.log(array.join(','));
+   }
+   
+   let array: number[] = [2, 3, 4];
+   
+   console.log(array[0]); // 2
+   
+   printArray(array.slice(1)); // 3,4
+   console.log(array[1], array[2]); // 3 4
+   
+   array.push(5);
+   printArray(array); // 3,4,5
+   console.log(array[array.length - 1]); // 5
+   array.pop();
+   printArray(array); // 3,4
+   
+   array.push(1);
+   printArray(array); // 3,4,1
+   
+   array.unshift(8);
+   printArray(array); // 8,3,4,1
+   
+   /** check if every number is greater than 3 */
+   let everyisgreater = array.every(num => num > 3);
+   console.log(everyisgreater); // false
+   
+   /** check if every number is less than 10 */
+   let everyisless = array.every(num => num < 10);
+   console.log(everyisless); // true
+   
+   console.log(array.slice().sort()); // 1,3,4,8
+   console.log(array.slice().sort((a, b) => b - a)); // 8,4,3,1
